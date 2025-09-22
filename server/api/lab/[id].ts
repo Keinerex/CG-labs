2c546df4cc9966eb88ab80e8db5a5312a7aff21b
keinerex <yurk0v-vl@yandex.ru>
2025-09-22 21:44:01 +0700

    Добавил лог, проверить сервер

--- server/api/lab/[id].ts
+++ server/api/lab/[id].ts
@@ -4,7 +4,15 @@ export default defineEventHandler<PromiseLabContent>(async (event) => {
 	const labId = getRouterParam(event, 'id');
 	const data = await useStorage('assets:labs').getItem<string>(`lab${labId}.ts`);
 
+	console.log(data);
+
+	if (!data) {
+		return {
+			content: [],
+		};
+	}
+
 	return {
-		content: data?.split('\n')?.filter(Boolean)?.slice(1, -1)?.map((item: string) => item.slice(1)) ?? [],
+		content: data.split('\n').filter(Boolean).slice(1, -1).map((item: string) => item.slice(1)) ?? [],
 	};
 });
